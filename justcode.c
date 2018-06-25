46628f87dc380fc092ff47e12e4b300c93fe073e
Stephen A. Wood <zviwood@gmail.com>
2018-06-25 14:46:05 -0400

    Fix just code so that it prints out prime numbers

--- justcode.c
+++ justcode.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAXPRIME 1000
 
@@ -8,10 +9,12 @@ int main(){
   int maxfactor;
   int factor;
   int j;
+  int i;
 
   printf("This is a program to compute prime nubmers using the\n");
   printf("Sieve of Eratosthenes\n");
 
+  /* Sieve of Eratosthenes */
   primes = malloc(sizeof(int)*MAXPRIME/2);
 
   for(i=3;i<MAXPRIME;i += 2) {
@@ -19,16 +22,22 @@ int main(){
     primes[index] = 1;
   }
 
-  maxfactor = sqrt(double(MAXPRIME));
+  maxfactor = sqrt((double) MAXPRIME);
 
   for(factor=3;factor<=maxfactor;factor += 2) {
     int index = (factor-3)/2;
     if(primes[index]) {		/* Only need to seive of prime */
-      for(j = factor;j*factor<MAXPRIME; j += factor) {
+      for(j = factor + 2*factor;j<MAXPRIME; j += 2*factor) {
 	int ind = (j-3)/2;
 	primes[ind] = 0;
       }
     }
   }
+  /* Print out the results */
+  for(i=3;i<MAXPRIME;i+=2) {
+    int index = (i-3)/2;
+    if(primes[index]) printf("%d ",i);
+  }
+  printf("\n");
 }
 	
