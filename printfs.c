a8f86527f2d134d28e52399f0217111f6f9c6924
Stephen A. Wood <zviwood@gmail.com>
2018-06-25 15:26:27 -0400

    printfs: Add computation of Lucas Numbers

--- printfs.c
+++ printfs.c
@@ -7,7 +7,7 @@ int main(){
 
   fn_last = 0;
   fn = 1;
-  printf("%d ",fn);
+  printf("%d %d ",fn_last,fn);
   while(fn < 2000) {
     int fn_new = fn_last + fn;
     fn_last = fn;
@@ -16,6 +16,18 @@ int main(){
   }
 
   printf("\nOK, it wasn't just printing, we did some calculating\n");
+
+  /* Do again with different starting numbers.  These are Lucas numbers */
+  fn_last = 2;
+  fn=1;
+  printf("%d %d ",fn_last,fn);
+  while(fn < 2000) {
+    int fn_new = fn_last + fn;
+    fn_last = fn;
+    fn = fn_new;
+    printf("%d ",fn);
+  }
+  
 }
 
     
